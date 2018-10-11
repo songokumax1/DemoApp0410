695f7fc367f0fb326196bad05268ee2a67114b34
VoHoangThien <thiendq215@gmail.com>
2018-10-11 07:59:53 +0700

    sua button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -57,7 +57,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "cong";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
